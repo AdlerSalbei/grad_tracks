82f7a1483df370cbc737556bef6fe4f75572992c
Salbei <karsus3@web.de>
2018-08-22 12:02:30 +0100

    travis test

--- addons/functions/CfgEventHandlers.hpp
+++ addons/functions/CfgEventHandlers.hpp
@@ -11,16 +11,9 @@ class Extended_PreInit_EventHandlers {
 };
 
 class Extended_Deleted_EventHandlers {
-  class grad_tracks_F {
-    deleted = QUOTE(if (isServer) then {_target setVariable [QGVAR(rearpack),objNull,true];});
-  };
+
 };
 
 class Extended_Init_EventHandlers {
-    class grad_tracks_F {
-        init = QUOTE(_this call FUNC(initApobs));
-    };
-    class grad_tracks_R {
-        init = QUOTE(_this call FUNC(initApobs));
-    };
+    
 };
